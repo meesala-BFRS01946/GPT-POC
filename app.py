2e6b0ee6889091397e79f8052853f526917e5d2a
meesala sai dhanush <meesala.dhanush@shiprocket.com>
2023-05-23 11:55:30 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import openai
-import pickle
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -8,14 +7,23 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader
 from langchain.document_loaders.csv_loader import CSVLoader
-import pandas as pd
+#import pandas as pd
+import chromadb
 
 load_dotenv()
 openai.api_key =os.getenv("OPENAI_API_KEY")
 
 app=Flask(__name__)
+
+
+loader = CSVLoader(file_path='./FAQ_text_data1.csv',csv_args={'delimiter': ','})
+data = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(texts, embeddings)
+qa = RetrievalQA.from_chain_type(llm=OpenAI(batch_size=5), chain_type="map_reduce", retriever=docsearch.as_retriever(search_kwargs={"k": 1}))
 
 @app.route('/',methods=['GET'])
 def home():
@@ -39,31 +47,22 @@ def prod_desc():
 @app.route('/text',methods=['POST'])
 def train_doc():
     query=request.get_json()
-   # loader = TextLoader("FAQ")
-  #  documents = loader.load()
-    loader = CSVLoader(file_path='./faq.csv',csv_args={'delimiter': ','})
-    data = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(data)
-    embeddings = OpenAIEmbeddings()
-    docsearch = Chroma.from_documents(texts, embeddings)
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}))
-    user_q=query["question"]
-    query = "give the answer to the user's query .The answers should be given from the Document provided to you and if there is no answer from that document please return 'sorry i don't know'.The user query is {}".format(user_q)
+    user_q=query["text"]
+    query = "give the answer to the user's query from the embeddings what you have got trained.The answers should be given from the Document provided to you  and if there is no answer from that document please return 'NULL'.Please ensure to provide the answer in bullet points.The user query is {}".format(user_q)
     return qa.run(query)
 
-@app.route('/update', methods=['POST'])
-def updateRow():
-    body = request.get_json()
-    file_path = 'faq.csv'
-    qa_id = body['qa_id']
-    column_name = body['column_name']
-    value = body['value']
-    df = pd.read_csv(file_path)
-    df.set_index('ID', inplace=True)
-    df.at[qa_id, column_name] = value
-    df.to_csv(file_path, index=True)
-    return "File Updated."
+# @app.route('/update', methods=['POST'])
+# def updateRow():
+#     body = request.get_json()
+#     file_path = 'faq.csv'
+#     qa_id = body['qa_id']
+#     column_name = body['column_name']
+#     value = body['value']
+#     df = pd.read_csv(file_path)
+#     df.set_index('ID', inplace=True)
+#     df.at[qa_id, column_name] = value
+#     df.to_csv(file_path, index=True)
+#     return "File Updated."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=int("3000"),debug=True)
